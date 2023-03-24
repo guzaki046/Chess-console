0882d05a26a88d2545d57a93d94c538ce1347390
Gustavo Nozaki Soares <gnozakisoares@gmail.com>
2023-03-24 17:51:02 -0300

    "Handling errors with Exceptions"

--- BoardGame/Board.cs
+++ BoardGame/Board.cs
@@ -1,4 +1,6 @@
-﻿namespace BoardGame
+﻿using BoardGame.Exceptions;
+
+namespace BoardGame
 {
     internal class Board
     {
@@ -18,10 +20,42 @@
             return pieces[line, column];
         }
 
+        public Piece piece(Position pos)
+        {
+            return pieces[pos.Line, pos.Column];
+        }
+
         public void PutPiece(Piece p, Position pos)
         {
+            if (ThereIsPiece(pos))
+            {
+                throw new BoardException("There is already a piece here");
+            }
             pieces[pos.Line, pos.Column] = p;
             p.position = pos;
+        }
+
+        public bool ThereIsPiece(Position pos)
+        {
+            ValidatePosition(pos);
+            return piece(pos) != null;
+        }
+
+        public bool ValidPosition(Position pos)
+        {
+            if (pos.Line < 0 || pos.Line >= Lines || pos.Column < 0 || pos.Column >= Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!ValidPosition(pos))
+            {
+                throw new BoardException("Invalid position!");
+            }
         }
     }
 }
---
+++ BoardGame/Exceptions/BoardException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BoardGame.Exceptions
+{
+    internal class BoardException : ApplicationException
+    {
+        public BoardException(string message) : base(message) { }
+    }
+}
